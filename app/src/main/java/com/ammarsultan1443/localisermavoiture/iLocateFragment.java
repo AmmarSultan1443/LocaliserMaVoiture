f2052ebed3d3f84e3607fa5e25a3ce7c47546cf7
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2023-03-30 16:57:25 +0000

    Working on searching feature

--- app/src/main/java/com/ammarsultan1443/localisermavoiture/iLocateFragment.java
+++ app/src/main/java/com/ammarsultan1443/localisermavoiture/iLocateFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,6 +17,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.ammarsultan1443.localisermavoiture.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class iLocateFragment extends Fragment {
     private ImageView earth;
@@ -26,6 +28,11 @@ public class iLocateFragment extends Fragment {
     private Button buttonValidate;
     private Button buttonCancel;
     private Button buttonMoreOptions;
+    private Button buttonSearchValidate;
+    private Button buttonSearchCancel;
+    private Button buttonSearchMoreOptions;
+    private Button buttonLookup;
+    private EditText editTextSearch;
     private ConstraintLayout constraintLayoutiLocate;
 
     @Override
@@ -47,9 +54,16 @@ public class iLocateFragment extends Fragment {
         buttonLocate=(Button)view.findViewById(R.id.imageButtonLocate);
         buttonSearch=(Button)view.findViewById(R.id.imageButtonSearch);
         buttonServiceCar=(Button)view.findViewById(R.id.imageButtonServiceCar);
+        //if buttonLocate clicked those buttons show up
         buttonValidate=(Button)view.findViewById(R.id.imageButtonValidate);
         buttonCancel=(Button)view.findViewById(R.id.imageButtonCancel);
         buttonMoreOptions=(Button)view.findViewById(R.id.imageButtonMoreOptions);
+        //if buttonSearch clicked then those buttons show up
+        buttonSearchValidate=(Button)view.findViewById(R.id.SearchValidate);
+        buttonSearchCancel=(Button)view.findViewById(R.id.SearchCancel);
+        buttonSearchMoreOptions=(Button)view.findViewById(R.id.SearchMoreOptions);
+        editTextSearch=(EditText)view.findViewById(R.id.editTextSearchBar);
+        buttonLookup=(Button)view.findViewById(R.id.lookup);
 
         buttonSettings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +87,17 @@ public class iLocateFragment extends Fragment {
                 buttonCancel.setVisibility(View.VISIBLE);
                 buttonMoreOptions.setVisibility(View.VISIBLE);
 
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonSearchValidate.setVisibility(View.VISIBLE);
+                buttonSearchCancel.setVisibility(View.VISIBLE);
+                buttonSearchMoreOptions.setVisibility(View.VISIBLE);
+                editTextSearch.setVisibility(View.VISIBLE);
+                buttonLookup.setVisibility(View.VISIBLE);
             }
         });
 
